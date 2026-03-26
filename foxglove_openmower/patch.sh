05bf575e2fa5ba35efffb2b12150f7ba21f479ae
Pepeuch <pepeuch@gmail.com>
2026-03-26 17:42:14 +0100

    adjust patching for build

--- foxglove_openmower/patch.sh
+++ foxglove_openmower/patch.sh
@@ -1,4 +1,26 @@
 #!/bin/bash
 set -xe
-echo "Patching OM"
-sed -i 's/output="screen"/unless="\$\(optenv OM_NO_COMMS True\)" output="screen"/g' /opt/open_mower_ros/src/open_mower/launch/include/_comms.launch
+echo "Patching for implausible wheel ticks"
+
+FILE="/opt/open_mower_ros/src/lib/xbot_positioning/src/xbot_positioning.cpp"
+LINE="vx = d_ticks / dt;"
+
+NEW_CODE='    // Ignore implausible wheel tick calculation
+    if(abs(vx) > 0.6) {
+        ROS_WARN_STREAM("got vx > 0.6 (" << vx << ") - dropping measurement");
+        vx = 0.0;
+        return;
+    }'
+
+if ! grep -q 'got vx > 0.6 (' "$FILE"; then
+    awk -v new_code="$NEW_CODE" -v line="$LINE" '
+    $0 ~ line {
+        print $0
+        print new_code
+        next
+    }
+    { print }
+    ' "$FILE" > /tmp/xbot_positioning.cpp && mv /tmp/xbot_positioning.cpp "$FILE"
+else
+    echo "Wheel tick patch already present, skipping."
+fi
